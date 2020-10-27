8a2f280476ffae7902fea7136fb69a7d26384141
masaKoodari <kdtsql@protonmail.com>
2020-10-27 10:45:07 +0200

    heya

--- js/guestBookApp.js
+++ js/guestBookApp.js
@@ -1,17 +1,31 @@
-const listaLisäys = function(){
-    let vierasValue = document.getElementById("vieras").value;
-    document.getElementById("vieraslista").innerHTML += "<li>" +vierasValue+ "</li>";
+//ARRAY OF UsER INPUT
+var arr = [];
+
+//DISPLAY MESSAGE AND ADD TO ARRAY
+function listaLisäys() {
+  var vierasValue = document.getElementById("vieras").value;
+  if (vierasValue == "") {
+    alert("Se oli tyhjä!");
+  }
+  else {
+    arr.push(vierasValue);
+    document.getElementById("vieraslista").innerHTML += "<li>" + vierasValue + "</li>";
     document.getElementById("vieras").value = "";
+  }
 }
 
+//SHOW ARRAY IN VIERASLISTA
+function jumalauta() {
+  document.getElementById("vieraslista").innerHTML += "<li>" + arr + "</li>";
+}
 
 /* navigointi palkin scripti */
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
